09162b0cfa4370ef892871b3700d0b9db3829651
John Vincent <jvincent@hotels.com>
2017-11-08 13:50:16 +0000

    package structure

--- api.go
+++ api.go
@@ -1,11 +1,26 @@
-package main
+package glock
 
 import (
+	"time"
+
 	"github.com/gocql/gocql"
 	"github.com/yehohanan7/glock/cassandra"
-	. "github.com/yehohanan7/glock/glock"
+	"github.com/yehohanan7/glock/glock"
 )
+
+type GlockConfig struct {
+	Owner    string
+	Ticker   *time.Ticker
+	Store    LockStore
+	MasterCh chan struct{}
+	slaveCh  chan struct{}
+	StopCh   chan struct{}
+}
 
 func NewCassandraStore(session *gocql.Session) LockStore {
 	return cassandra.NewStore(session)
+}
+
+func StartGlock(config GlockConfig) {
+	glock.Start(config.Owner, config.Ticker, config.Store, config.MasterCh, config.SlaveCh, config.StopCh)
 }
---
+++ glock.go
@@ -0,0 +1,33 @@
+package glock
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+var LockOwnershipLost = errors.New("lock is owned by other owner")
+
+type Lock struct {
+	Owner string
+}
+
+type LockStore interface {
+	AcquireLock(owner string) (Lock, error)
+	GetLock() (Lock, error)
+	RenewLock(owner string) (Lock, error)
+	Clear() error
+}
+
+func Start(owner string, ticker *time.Ticker, lockStore LockStore, master, slave, stop chan struct{}) error {
+	for {
+		select {
+		case <-ticker.C:
+			glog.Info("running election")
+		case <-stop:
+			glog.Info("stoping glock...")
+			return nil
+		}
+	}
+}
